b26371ad1912bc9184d0b0d673ecd8a6d3a241f0
Aryan <aryan.soni@btech.christuniversity.in>
2020-04-01 19:50:48 +0530

    routes and templating engine added (initial)

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+app.get('/home', (req, res) => {
+    res.render('home', {
+        'title': 'Welcome To Story Books',
+        'body': 'Get Ready To Write!'
+    });
+});
+
+app.post('/login', (req, res) => {
+    res.render('login', {
+        'title': 'Login | Welcome!'
+    });
+});
+
+app.use('', (req, res) => {
+    res.render('notFound', {
+        'title': 'Page Not Found',
+        'body': '404 Error'
+    });
+});
+
+
+port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${[port]}`);
+});
