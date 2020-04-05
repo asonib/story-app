aed1ccf5ba273ec26ab69de4f19f40f92360ee64
Aryan Soni <aryan85tata@gmail.com>
2020-04-06 00:19:21 +0530

    added controllers for register

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,25 @@
+const mongoose = require('mongoose');
+require('../models/Register')
+const Register = mongoose.model('register');
+
 exports.addRegister = (req, res) => {
     res.render('register', {
         'title': 'Register | Welcome!'
+    });
+}
+
+exports.postRegister = (req, res) => {
+    console.log(req.body);
+    const newUser = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }
+    const newData = new Register(newUser);
+    newData.save()
+    .then(() => {
+        res.render('login', {
+            title: 'Login'
+        });
     });
 }
--- routes/register.js
+++ routes/register.js
@@ -2,26 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-require('../models/Register')
-const Register = mongoose.model('register');
 
 const registerRoute = require('../controllers/register');
 
 router.get('/register', registerRoute.addRegister);
-router.post('/register', (req, res) => {
-    console.log(req.body);
-    const newUser = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }
-    const newData = new Register(newUser);
-    newData.save()
-    .then(() => {
-        res.render('login', {
-            title: 'Login'
-        });
-    });
-});
+router.post('/register', registerRoute.postRegister);
 
 module.exports = router;
